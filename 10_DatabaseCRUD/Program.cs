1f5c7b3fa07a508d862a42010fcdcfc6147c3eca
UgurNlbnt <ugur.nalbant617@gmail.com>
2025-12-21 18:52:05 +0300

    C#_camp_301

--- 10_DatabaseCRUD/Program.cs
+++ 10_DatabaseCRUD/Program.cs
@@ -121,6 +121,7 @@ namespace _10_DatabaseCRUD
             Console.WriteLine("Ürün Güncelleme İşlemi Tamamlandı.");
 
             #endregion
+
             Console.Read();
         }
     }
---
+++ C#Egitim_Campi_301/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Egitim_Campi_301
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
